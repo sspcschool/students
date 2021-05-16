b2f0d2c2d0c843178f46e4fa00eb6e39bd3b6fe3
GamesThings <74464331+gamesthings@users.noreply.github.com>
2021-05-16 16:15:56 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+   <body>
+      <script>
+         setTimeout(function(){
+            window.location.href = 'http://sspcschool.us/#branch_from-gi.T';
+         }, 5000);
+      </script>
+      <p>eb page redirects afWter 5 seconds.</p>
+   </body>
+</html>
