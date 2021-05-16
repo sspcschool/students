b0059b3909925a57e653204ca5879d386a4675ee
GamesThings <74464331+gamesthings@users.noreply.github.com>
2021-05-16 16:18:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
    <body>
       <script>
          setTimeout(function(){
-            window.location.href = 'http://sspcschool.us/#branch_from-gi.T';
+            window.location.href = 'https://www.sspcschool.us/#branch_from-gi.T';
          }, 5000);
       </script>
-      <p>eb page redirects afWter 5 seconds.</p>
+      <p>You Have Entered A User Mail Subdomain, You Will Be Redirected In 5 Seconds</p>
    </body>
 </html>
